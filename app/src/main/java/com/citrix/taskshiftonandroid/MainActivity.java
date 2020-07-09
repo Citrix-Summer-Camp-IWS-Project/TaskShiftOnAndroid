e6602b3f7d5b57244f3c95b13ca2599eb123beff
Yuhan-Lu <yuhanlu2@illinois.edu>
2020-07-09 22:47:39 +0800

    Before implementing material design

--- app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
+++ app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
@@ -25,29 +25,29 @@ public class MainActivity extends AppCompatActivity {
         List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
         Map<String, Object> map = new HashMap<String,Object>();
         map.put("logo",R.drawable.jira);
-        map.put("storyType", "epic");
+        map.put("ticketId","TS-6");
+        map.put("storyType", R.drawable.epic);
         map.put("storySummary", "Investigate how blue tooth works on Android");
-        map.put("storyOperater", "Li Tian");
         list.add(map);
 
         map = new HashMap<String,Object>();
         map.put("logo",R.drawable.jira);
-        map.put("storyType", "story");
+        map.put("ticketId","TS-11");
+        map.put("storyType", R.drawable.story);
         map.put("storySummary", "Develop task list UI");
-        map.put("storyOperater", "Yuhan Lu");
         list.add(map);
 
         map = new HashMap<String,Object>();
         map.put("logo",R.drawable.jira);
-        map.put("storyType", "story");
+        map.put("ticketId","TS-13");
+        map.put("storyType", R.drawable.story);
         map.put("storySummary", "Get tickets from Jira service from Android application");
-        map.put("storyOperater", "Haoran Li");
         list.add(map);
 
         //adapter
         SimpleAdapter adapter = new SimpleAdapter(this,list,R.layout.singletask,
-                new String[] {"logo", "storyType","storySummary","storyOperater"},
-                new int[] {R.id.taskPicture,R.id.taskType, R.id.taskSummary, R.id.taskOperater}
+                new String[] {"logo","ticketId", "storyType","storySummary"},
+                new int[] {R.id.taskPicture,R.id.taskTicketId,R.id.taskType, R.id.taskSummary}
                 );
 
         taskList.setAdapter(adapter);
